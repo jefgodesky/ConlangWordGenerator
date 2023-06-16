015a716948e3be284b454809614733b7594a60e2
Jason Godesky <jefgodesky@gmail.com>
2023-06-15 20:46:55 -0400

    Language: Constructor loads vowels from YAML file

--- src/language/cls.py
+++ src/language/cls.py
@@ -6,3 +6,4 @@ class Language:
         with open(filename) as file:
             contents = yaml.safe_load(file)
         self.name = contents['name']
+        self.vowels = contents['vowels']
--- src/language/tests.py
+++ src/language/tests.py
@@ -7,5 +7,8 @@ class TestLanguage(unittest.TestCase):
         self.filename = 'test.yml'
         self.lang = Language(self.filename)
 
-    def test_init_load(self):
+    def test_init_load_name(self):
         self.assertEqual(self.lang.name, 'Test')
+
+    def test_init_load_vowels(self):
+        self.assertEqual(len(self.lang.vowels), 5)
