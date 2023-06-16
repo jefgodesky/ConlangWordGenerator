6d3b9b028db86a1e7e4eb23be0d1f052ee470ffd
Jason Godesky <jefgodesky@gmail.com>
2023-06-16 12:56:47 -0400

    List languages

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-yaml = input("Language file: ")
-print(yaml)
+import os
+
+from src.language.cls import Language
+
+
+def load_languages(lang_dir):
+    lang_list = []
+    for filepath in os.listdir(lang_dir):
+        if os.path.isfile(os.path.join(lang_dir, filepath)) and filepath.endswith('.yml') and filepath != 'test.yml':
+            lang = Language(f'{lang_dir}/{filepath}')
+            lang_list.append(lang)
+    return lang_list
+
+
+if __name__ == '__main__':
+    languages = load_languages('languages')
+    for lang in languages:
+        print(lang.name)
