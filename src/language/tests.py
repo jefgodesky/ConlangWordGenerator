4f7c9140a6279ed48bc682652fe1133393e92d25
Jason Godesky <jefgodesky@gmail.com>
2023-06-16 07:27:24 -0400

    Language: Refactor tests

--- src/language/tests.py
+++ src/language/tests.py
@@ -11,25 +11,25 @@ class TestLanguage(unittest.TestCase):
         self.assertEqual(self.lang.name, 'Test')
 
     def test_init_load_vowels(self):
-        self.assertEqual(len(self.lang.vowels), 1)
+        self.assertEqual(self.lang.vowels, {'a': 'a'})
 
     def test_init_load_consonants(self):
-        self.assertEqual(len(self.lang.consonants), 2)
+        self.assertEqual(self.lang.consonants, {'b': 'b', 'c': 'c'})
 
     def test_init_load_clusters(self):
-        self.assertEqual(len(self.lang.clusters), 1)
+        self.assertEqual(self.lang.clusters, {'bc': 'bc'})
 
     def test_init_load_onset_incidence(self):
         self.assertEqual(self.lang.syllables['onset']['incidence'], 0.5)
 
     def test_init_load_onset_options(self):
-        self.assertEqual(len(self.lang.syllables['onset']['options']), 3)
+        self.assertEqual(self.lang.syllables['onset']['options'], ['a', 'b'])
 
     def test_init_load_syllable_forbidden_patterns(self):
-        self.assertEqual(len(self.lang.syllables['forbidden']), 1)
+        self.assertEqual(self.lang.syllables['forbidden'], ['CCVCC'])
 
     def test_get_all_consonant_options(self):
-        self.assertEqual(len(self.lang.get_all_consonant_options()), 3)
+        self.assertEqual(self.lang.get_all_consonant_options(), {'b': 'b', 'c': 'c', 'bc': 'bc'})
 
     def test_pick_random_sound(self):
         sounds = {'a': 1, 'b': 2}
