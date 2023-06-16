83e6465e459c11a256d1e05a8a99eb100d953fb0
Jason Godesky <jefgodesky@gmail.com>
2023-06-16 13:17:52 -0400

    Prompt user for options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import inquirer
 
 from src.language.cls import Language
 
@@ -12,7 +13,27 @@ def load_languages(lang_dir):
     return lang_list
 
 
+def get_language(choice, lang_list):
+    for lang in lang_list:
+        if choice == lang.name:
+            return lang
+    return None
+
+
+
 if __name__ == '__main__':
     languages = load_languages('languages')
-    for lang in languages:
-        print(lang.name)
+    options = [
+        inquirer.List('language',
+                      message='Pick a language:',
+                      choices=list(map(lambda language: language.name, languages))
+                      ),
+        inquirer.Text('words',
+                      message='Words to generate: ',
+                      validate=lambda _, x: x.isdigit())
+    ]
+    answers = inquirer.prompt(options)
+
+    language = get_language(answers['language'], languages)
+    words = language.generate_words(int(answers['words']))
+    csv = language.create_csv(words)
